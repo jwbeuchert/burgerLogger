c16a5332d04aa5f7222d76e2198a73b855e2f3b5
Jacob Beuchert <jacobbeuchert@gmail.com>
2020-02-01 10:05:21 -0700

    Commenting out some connection code

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 var mysql = require("mysql");
-var connection
+var connection;
 
 if(process.env.JAWSDB_URL) {
 connection= mysql.createConnection(process.env.JAWSDB_URL)
@@ -13,13 +13,13 @@ connection= mysql.createConnection(process.env.JAWSDB_URL)
   })
 };
 
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect()
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 module.exports = connection;
